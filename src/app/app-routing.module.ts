57ece972641e08b20004eb4dea6624fc873ff0bc
German <gerudaeta@gmail.com>
2019-08-05 01:12:07 -0300

    add componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
+    { path: 'alert', loadChildren: './pages/alert/alert.module#AlertPageModule' },
+    { path: 'action-sheet', loadChildren: './pages/action-sheet/action-sheet.module#ActionSheetPageModule' },
+    { path: 'avatar', loadChildren: './pages/avatar/avatar.module#AvatarPageModule' },
+    { path: 'botones', loadChildren: './pages/botones/botones.module#BotonesPageModule' },
+  { path: 'card', loadChildren: './pages/card/card.module#CardPageModule' },
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/alert/alert.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+    selector: 'app-alert',
+    templateUrl: './alert.page.html',
+    styleUrls: ['./alert.page.scss'],
+})
+export class AlertPage implements OnInit {
+
+    titulo: string;
+
+    constructor( private alertController: AlertController ) { }
+
+    ngOnInit() {
+    }
+
+    async presentInput() {
+        const input = await this.alertController.create({
+            header: 'Input',
+            subHeader: 'Ingrese su nombre:',
+            inputs: [
+                {
+                    name: 'txtNombre',
+                    type: 'text',
+                    placeholder: 'Nombre'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancelar',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Confirm Cancel');
+                    }
+                }, {
+                    text: 'OK',
+                    handler: ( data ) => {
+                        console.log('Confirm Ok', data);
+                        this.titulo = data.txtNombre;
+                    }
+                }
+            ]
+        });
+        await input.present();
+    }
+
+    async presentAlert() {
+        const alert = await this.alertController.create({
+            header: 'Alert',
+            subHeader: 'Subtitle',
+            message: 'This is an alert message.',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                        console.log('Cancelar');
+                    }
+                },
+                {
+                    text: 'Ok',
+                    handler: (blah) => {
+                        console.log('Botón OK');
+                    }
+                }
+            ]
+        });
+        await alert.present();
+    }
+
+}
